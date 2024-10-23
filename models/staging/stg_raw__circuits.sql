0f1d2aa49d5f1e264fd4f1c182c7cea0989dbaab
Noah Fisher <noahfisheranalytics@gmail.com>
2024-10-23 18:58:36 +0000

    Created staging files

---
+++ models/staging/stg_raw__circuits.sql
@@ -0,0 +1,10 @@
+select
+    circuitid as circuit_id,
+    circuitref as circuit_ref,
+    circuitname as circuit_name,
+    location,
+    country,
+    latitude,
+    longitude
+
+from formula1.raw.raw_circuits
